32f72eb309e0dc82d9508e70a933752ae4db76ed
Chad Saglam <saglam.chd@gmail.com>
2020-09-29 13:27:09 +0200

    tweet mode, export tweet-user-info to indexjs &small styling

--- js/index.js
+++ js/index.js
@@ -26,6 +26,7 @@ const getTwitterData = () => {
     return response.json();
   }).then((data) => {
     console.log(data)
+    return buildTweets(data.statuses);
   });
 }
 
@@ -53,13 +54,59 @@ const nextPageButtonVisibility = (metadata) => {
  * Build Tweets HTML based on Data from API
  */
 const buildTweets = (tweets, nextPage) => {
-
+  let twitterContent = "";
+  tweets.map((tweet) => {
+    twitterContent += `
+      <div class="tweet-container">
+          <div class="tweet-user-info">
+              <div class="tweet-user-profile" style="background-image: url(${tweet.user.profile_image_url_https})"></div>
+              <div class="tweet-user-name-container">
+                  <div class="tweet-user-fullname">${tweet.user.name}</div>
+                  <div class="tweet-user-username">@${tweet.user.screen_name}</div>
+              </div>
+          </div>
+    `
+    if (tweet.extended_entities
+      && tweet.extended_entities.media
+      && tweet.extended_entities.media.length > 0) {
+      twitterContent += buildImages(tweet.extended_entities.media);
+      twitterContent += buildVideo(tweet.extended_entities.media);
+    }
+    twitterContent += `
+          <div class="tweet-text-container">
+            <span class="tweet-text">
+            ${tweet.full_text}
+            </span>
+          </div>
+          <div class="tweet-date">
+            2min ago
+          </div>
+      </div>
+    `
+  })
+  if (nextPage) {
+    document.querySelector('.tweets-list').insertAdjacentHTML('beforeend', twitterContent)
+  } else {
+    document.querySelector('.tweets-list').innerHTML = twitterContent;
+  }
 }
 
 /**
  * Build HTML for Tweets Images
  */
 const buildImages = (mediaList) => {
+  let imagesContent = `<div class="tweet-images-container">`;
+  let imagesExist = false;
+  mediaList.map((media) => {
+    if (media.type == "photo") {
+      imagesExist = true;
+      imagesContent += `
+        <div class="tweet-image" style="background-image: url(${media.media_url_https})"></div>
+      `
+    }
+  })
+  imagesContent += `</div>`;
+  return (imagesExist ? imagesContent : '');
 
 }
 
@@ -67,5 +114,29 @@ const buildImages = (mediaList) => {
  * Build HTML for Tweets Video
  */
 const buildVideo = (mediaList) => {
+  let videoContent = `<div class="tweet-video-container">`;
+  let videoExists = false;
+  mediaList.map((media) => {
+    if (media.type == "video" || media.type == 'animated_gif') {
+      videoExists = true;
+      const video = media.video_info.variants.find((video) => video.content_type == 'video/mp4');
+      const videoOptions = getVideoOptions(media.type);
+      videoContent += `
+        <video ${videoOptions}>
+          <source src="${video.url}" type="video/mp4">
+          Your browser does not support HTML5 video.
+        </video>
+      `
+    }
+  })
+  videoContent += `</div>`;
+  return (videoExists ? videoContent : '');
+}
 
+const getVideoOptions = (mediaType) => {
+  if (mediaType == 'animated_gif') {
+    return "loop autoplay";
+  } else {
+    return "controls";
+  }
 }
